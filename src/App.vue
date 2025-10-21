078d2179647143a8c9d7c1b6421bfac54a73447e
FridayBlessed <Fridayblessed333@gmail.com>
2025-10-21 01:54:59 +0100

    assessment 3

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+
+<style>
+/* global styles */
+</style>
+
+
+<script setup lang="ts"></script>
+<style src="./App.css"></style>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="not-found">
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you're looking for doesn't exist.</p>
+    <router-link to="/">Go back home</router-link>
+  </main>
+</template>
+
+<script setup lang="ts">
+</script>
+
+
+
+
